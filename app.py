3ba66f2c5662455891cd41514ced91dcf23d41c6
Trisolis <papijackwaiter@gmail.com>
2026-08-13 20:48:29 -0400

    Finishing country specific logic
    Add necessary indicators to the country specific page
    Fix small syntax issues in other templates

--- app.py
+++ app.py
@@ -14,11 +14,13 @@ def get_db():
 def home():
     return render_template('index.html')
 
+'''
 # Accepts query parameter and searches countries by name (should redirect to /countries?region=Europe or something similar)
 @app.route('/search?q=<something>')
 def search():
     # code here
     pass
+'''
 
 # Lists all countries, perhaps filtered by region
 @app.route('/countries')
@@ -33,21 +35,19 @@ def countries():
 def country(iso_code):
     conn = get_db()
     results = conn.execute('''
-        SELECT c.*, i.population, i.birth_rate, i.death_rate, i.life_expectancy, i.literacy_rate, i.density, i.hdi, e.gdp_ppp,
-               e.gdp_per_capita, e.inflation, e.unemployment, e.public_debt_pct, e.gdp
+        SELECT c.*, i.population, i.life_expectancy, i.literacy_rate, i.hdi, e.gdp_per_capita, e.inflation, e.unemployment, e.gdp
         FROM countries c
         LEFT JOIN indicators i ON c.iso_code=i.iso_code
         LEFT JOIN economy e ON c.iso_code=e.iso_code
-        WHERE c.iso_code == ?
+        WHERE c.iso_code = ?
         ''', (iso_code,)).fetchone()
     conn.close()
     return render_template('country.html', country=results)
 
 @app.route('/cities')
 def cities():
+    conn = get_db()
     return render_template('cities.html')
-# Still need a 'country' route and a 'cities' route, probably a specific city/cities from x country route too
-
 
 if __name__ == '__main__':
     app.run(debug=True) # remove before deploying
